a9484efd4ff2b2a570ea8d47dc21ee0dfb998c8f
Ed Merks <ed.merks@gmail.com>
2014-07-28 16:34:01 +0200

    [440561] Proxy preferences dialog chops the first table if the second table has many entries.
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=440561

--- plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/ProxyPreferenceDialog.java
+++ plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/ProxyPreferenceDialog.java
@@ -14,9 +14,15 @@ import org.eclipse.oomph.util.ReflectUtil;
 
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.internal.net.ProxyPreferencePage;
 
@@ -58,38 +64,89 @@ public class ProxyPreferenceDialog extends AbstractPreferenceDialog
     @Override
     protected Control createContents(Composite parent)
     {
-      Composite composite = new Composite(parent, SWT.NONE);
+      final Composite mainComposite = new Composite(parent, SWT.NONE);
+      mainComposite.setLayout(new GridLayout(1, false));
+      mainComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+      final ScrolledComposite scrolledComposite = new ScrolledComposite(mainComposite, SWT.VERTICAL);
+      scrolledComposite.setExpandHorizontal(true);
+      scrolledComposite.setExpandVertical(true);
+      scrolledComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+      final Composite composite = new Composite(scrolledComposite, SWT.NONE);
       GridLayout layout = new GridLayout(1, false);
       layout.marginWidth = 5;
       layout.marginHeight = 5;
       composite.setLayout(layout);
 
-      createProviderComposite(composite);
-      createProxyEntriesComposite(composite);
-      createNonProxiedHostsComposite(composite);
+      scrolledComposite.setContent(composite);
+      composite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+      final int providerHeight = createProviderComposite(composite);
+      final int proxyEntriesHeight = createProxyEntriesComposite(composite);
+      createNonProxiedHostsComposite(composite, proxyEntriesHeight);
 
       applyDialogFont(composite);
       initializeValues();
 
+      ControlAdapter resizeListener = new ControlAdapter()
+      {
+        int height = providerHeight + 2 * proxyEntriesHeight - 10;
+
+        @Override
+        public void controlResized(ControlEvent event)
+        {
+          int width = mainComposite.getClientArea().x;
+          Point size = composite.computeSize(width, height - 10);
+          scrolledComposite.setMinSize(size);
+        }
+      };
+
+      scrolledComposite.addControlListener(resizeListener);
+      composite.addControlListener(resizeListener);
+      composite.notifyListeners(SWT.Resize, new Event());
+
       return composite;
     }
 
-    private void createProviderComposite(Composite composite)
+    private int createProviderComposite(Composite composite)
     {
       Method method = ReflectUtil.getMethod(ProxyPreferencePage.class, "createProviderComposite", Composite.class);
       ReflectUtil.invokeMethod(method, this, composite);
+      Control control = getLastChild(composite);
+      Point size = control.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+      return size.y;
     }
 
-    private void createProxyEntriesComposite(Composite composite)
+    private int createProxyEntriesComposite(Composite composite)
     {
       Method method = ReflectUtil.getMethod(ProxyPreferencePage.class, "createProxyEntriesComposite", Composite.class);
       ReflectUtil.invokeMethod(method, this, composite);
+
+      Control control = getLastChild(composite);
+      Point size = control.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+      GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, false);
+      layoutData.minimumHeight = size.y;
+      control.setLayoutData(layoutData);
+      return size.y;
     }
 
-    private void createNonProxiedHostsComposite(Composite composite)
+    private Control getLastChild(Composite composite)
+    {
+      Control[] children = composite.getChildren();
+      Control control = children[children.length - 1];
+      return control;
+    }
+
+    private void createNonProxiedHostsComposite(Composite composite, int height)
     {
       Method method = ReflectUtil.getMethod(ProxyPreferencePage.class, "createNonProxiedHostsComposite", Composite.class);
       ReflectUtil.invokeMethod(method, this, composite);
+
+      Control control = getLastChild(composite);
+      GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+      layoutData.minimumHeight = height;
+      control.setLayoutData(layoutData);
     }
 
     private void initializeValues()
