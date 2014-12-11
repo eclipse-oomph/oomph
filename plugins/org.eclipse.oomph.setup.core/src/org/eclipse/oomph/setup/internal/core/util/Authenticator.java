377e82d5516b1f6aff313ce42a6814d190b86452
Eike Stepper <stepper@esc-net.de>
2014-12-11 13:12:59 +0100

    [Releng] Fix copyrights

--- plugins/org.eclipse.oomph.setup.core/src/org/eclipse/oomph/setup/internal/core/util/Authenticator.java
+++ plugins/org.eclipse.oomph.setup.core/src/org/eclipse/oomph/setup/internal/core/util/Authenticator.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2014 Eike Stepper (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ */
 package org.eclipse.oomph.setup.internal.core.util;
 
 import org.eclipse.oomph.base.Annotation;
