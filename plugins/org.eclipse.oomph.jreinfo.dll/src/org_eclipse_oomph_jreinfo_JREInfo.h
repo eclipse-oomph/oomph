f6b6b36442f96ae1f41fbd4750b537d1ac6555c9
Eike Stepper <stepper@esc-net.de>
2015-04-09 17:40:38 +0200

    [463928] [Windows] Installer does not proceed when JRE not found.
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=463928

--- plugins/org.eclipse.oomph.jreinfo.dll/src/org_eclipse_oomph_jreinfo_JREInfo.h
+++ plugins/org.eclipse.oomph.jreinfo.dll/src/org_eclipse_oomph_jreinfo_JREInfo.h
@@ -6,12 +6,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#ifdef BIT32
+#define JNI_FUNCTION _Java_org_eclipse_oomph_jreinfo_JREInfo_getAllWin
+#else
+#define JNI_FUNCTION Java_org_eclipse_oomph_jreinfo_JREInfo_getAllWin
+#endif
+
 /*
  * Class:     org_eclipse_oomph_jreinfo_JREInfo
  * Method:    getAllWin
  * Signature: ()Lorg/eclipse/oomph/jreinfo/JREInfo;
  */
-JNIEXPORT jobject JNICALL Java_org_eclipse_oomph_jreinfo_JREInfo_getAllWin
+JNIEXPORT jobject JNICALL JNI_FUNCTION
   (JNIEnv *, jclass);
 
 #ifdef __cplusplus
