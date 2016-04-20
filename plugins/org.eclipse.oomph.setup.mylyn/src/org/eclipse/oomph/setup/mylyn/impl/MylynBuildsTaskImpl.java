8e8fc21a40bc518aa7bcd217a8d8355574451e95
Jaxsun McCarthy Huggan <jaxsun.mccarthy@tasktop.com>
2016-04-19 20:59:11 -0700

    [491625] decrypt Mylyn Builds password before using in all situations
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=491625
    Change-Id: Ie26d5af1c80669bd5919c2c82978bc30907b0ad1
    Signed-off-by: Jaxsun McCarthy Huggan <jaxsun.mccarthy@tasktop.com>

--- plugins/org.eclipse.oomph.setup.mylyn/src/org/eclipse/oomph/setup/mylyn/impl/MylynBuildsTaskImpl.java
+++ plugins/org.eclipse.oomph.setup.mylyn/src/org/eclipse/oomph/setup/mylyn/impl/MylynBuildsTaskImpl.java
@@ -451,7 +451,7 @@ public class MylynBuildsTaskImpl extends SetupTaskImpl implements MylynBuildsTas
     if (isAuthenticate())
     {
       UserCredentials credentials = server.getLocation().getCredentials(AuthenticationType.REPOSITORY);
-      if (credentials == null || !ObjectUtil.equals(credentials.getPassword(), getPassword()))
+      if (credentials == null || !ObjectUtil.equals(credentials.getPassword(), PreferencesUtil.decrypt(getPassword())))
       {
         return true;
       }
@@ -471,6 +471,7 @@ public class MylynBuildsTaskImpl extends SetupTaskImpl implements MylynBuildsTas
         String connectorKind = getConnectorKind();
 
         IBuildServer server = getServer();
+        String password = PreferencesUtil.decrypt(getPassword());
         if (server != null)
         {
           if (isAuthenticate())
@@ -483,7 +484,6 @@ public class MylynBuildsTaskImpl extends SetupTaskImpl implements MylynBuildsTas
         {
           String serverURL = getServerURL();
           String userID = getUserID();
-          String password = PreferencesUtil.decrypt(getPassword());
 
           context.log("Adding " + connectorKind + " server: " + serverURL);
 
