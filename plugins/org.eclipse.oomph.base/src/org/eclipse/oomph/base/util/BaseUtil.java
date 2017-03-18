jhribal/eclipse-oopmh-mirror

[512834] Refactor code from Utilities working set after SonarQube analysis.
https://bugs.eclipse.org/bugs/show_bug.cgi?id=512834


Signed-off-by: William Enright <william.enright@ericsson.com>

plugins/org.eclipse.oomph.base/src/org/eclipse/oomph/base/util/BaseUtil.java
@@ -81,11 +81,12 @@ public final class BaseUtil
 
   public static BaseResource loadResourceSafely(ResourceSet resourceSet, URI uri)
   {
+
     try
     {
       return (BaseResource)resourceSet.getResource(uri, true);
     }
-    catch (Throwable ex)
+    catch (IORuntimeException ex)
     {
       // This method is always expected to return a non-null result.
       BaseResource resource = (BaseResource)resourceSet.getResource(uri, false);
@@ -165,7 +166,7 @@ public final class BaseUtil
     }
     catch (IOException ex)
     {
-      throw new IORuntimeException();
+      throw new IORuntimeException(ex);
     }
   }
 

eclipse-oomph/oomph

Change-Id: I4c17fe0617130bec8e4a96ece9c23e7c3c2c6e7b

plugins/org.eclipse.oomph.base/src/org/eclipse/oomph/base/util/BaseUtil.java
@@ -85,7 +85,7 @@ public final class BaseUtil
     {
       return (BaseResource)resourceSet.getResource(uri, true);
     }
-    catch (Throwable ex)
+    catch (RuntimeException ex)
     {
       // This method is always expected to return a non-null result.
       BaseResource resource = (BaseResource)resourceSet.getResource(uri, false);
@@ -165,7 +165,7 @@ public final class BaseUtil
     }
     catch (IOException ex)
     {
-      throw new IORuntimeException();
+      throw new IORuntimeException(ex);
     }
   }
 