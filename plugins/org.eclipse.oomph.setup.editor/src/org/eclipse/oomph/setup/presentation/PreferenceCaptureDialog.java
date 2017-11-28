7bc2b7182389c423c9af0e173b88ccbf4e0567ed
Adrian Price <aprice@tibco.com>
2017-11-28 18:01:52 +0000

    [522127] Capture Preferences dialog now has a 'defaults' filter
    - Added a toolbar with filter button, which when active hides those
      preferences which are set to their default value
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=522127
    Signed-off-by: Adrian Price <aprice@tibco.com>

--- plugins/org.eclipse.oomph.setup.editor/src/org/eclipse/oomph/setup/presentation/PreferenceCaptureDialog.java
+++ plugins/org.eclipse.oomph.setup.editor/src/org/eclipse/oomph/setup/presentation/PreferenceCaptureDialog.java
@@ -32,6 +32,16 @@ import org.eclipse.emf.edit.provider.ItemProvider;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IPreferenceNodeVisitor;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.DoubleClickEvent;
@@ -40,6 +50,8 @@ import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -56,9 +68,14 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
+
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -218,7 +235,7 @@ public class PreferenceCaptureDialog extends Dialog
 
     PatternFilter filter = new PatternFilter();
     filter.setIncludeLeadingWildcard(true);
-    final FilteredTree availablePreferencesTree = new FilteredTree(availablePreferencesComposite, SWT.MULTI | SWT.BORDER, filter, true);
+    final FilteredTree availablePreferencesTree = new PreferenceFilteredTree(availablePreferencesComposite, SWT.MULTI | SWT.BORDER, filter);
     GridData availablePreferencesTreeGridData = new GridData();
     availablePreferencesTreeGridData.widthHint = Display.getCurrent().getBounds().width / 5;
     availablePreferencesTreeGridData.heightHint = Display.getCurrent().getBounds().height / 3;
@@ -579,6 +596,311 @@ public class PreferenceCaptureDialog extends Dialog
       }
 
       return result.toArray();
+    }
+  }
+
+  /**
+  * A filtered tree for displaying preferences, with an additional filter to suppress preferences set to their default value.
+  *
+  * @author Adrian Price <aprice@tibco.com>
+  */
+  private static final class PreferenceFilteredTree extends FilteredTree
+  {
+    private static final IScopeContext DUMMY_CONFIGURATION_SCOPE = new DummyScope(ConfigurationScope.SCOPE);
+
+    private static final IScopeContext DUMMY_DEFAULT_SCOPE = new DummyScope(DefaultScope.SCOPE);
+
+    private static final IScopeContext DUMMY_INSTANCE_SCOPE = new DummyScope(InstanceScope.SCOPE);
+
+    private static final IEclipsePreferences DUMMY_PREFERENCES = new DummyPreferences();
+
+    private static final IScopeContext DUMMY_PROJECT_SCOPE = new DummyScope(ProjectScope.SCOPE);
+
+    private ToolItem defaultsToolItem;
+
+    public PreferenceFilteredTree(Composite parent, int treeStyle, PatternFilter filter)
+    {
+      super(parent, treeStyle, filter, true);
+    }
+
+    @Override
+    protected Composite createFilterControls(Composite parent)
+    {
+      ((GridLayout)parent.getLayout()).numColumns = 3;
+      Composite filters = super.createFilterControls(parent);
+
+      ToolBar toolBar = new ToolBar(parent, SWT.FLAT | SWT.HORIZONTAL);
+      defaultsToolItem = new ToolItem(toolBar, SWT.CHECK);
+      defaultsToolItem.setImage(SetupEditorPlugin.INSTANCE.getSWTImage("filter_advanced_properties"));
+      defaultsToolItem.setToolTipText("Hide preferences set to their default value");
+      defaultsToolItem.addSelectionListener(new SelectionAdapter()
+      {
+        @Override
+        public void widgetSelected(SelectionEvent e)
+        {
+          treeViewer.refresh();
+        }
+      });
+
+      return filters;
+    }
+
+    @Override
+    protected Control createTreeControl(Composite parent, int style)
+    {
+      Control treeControl = super.createTreeControl(parent, style);
+      treeViewer.addFilter(new ViewerFilter()
+      {
+
+        private final IPreferencesService preferencesService = Platform.getPreferencesService();
+
+        private final IScopeContext[] defaultContexts = new IScopeContext[] { DUMMY_PROJECT_SCOPE, DUMMY_INSTANCE_SCOPE, DUMMY_CONFIGURATION_SCOPE,
+            DefaultScope.INSTANCE };
+
+        private final IScopeContext[] preferenceContexts = new IScopeContext[] { DUMMY_PROJECT_SCOPE, DUMMY_INSTANCE_SCOPE, DUMMY_CONFIGURATION_SCOPE,
+            DUMMY_DEFAULT_SCOPE };
+
+        private final Map<PreferenceTask, Boolean> selected = new HashMap<PreferenceTask, Boolean>();
+
+        @Override
+        public boolean select(Viewer viewer, Object parentElement, Object element)
+        {
+          if (!defaultsToolItem.getSelection())
+          {
+            return true;
+          }
+
+          if (element instanceof CompoundTask)
+          {
+            CompoundTask compoundTask = (CompoundTask)element;
+            EList<SetupTask> setupTasks = compoundTask.getSetupTasks();
+            for (Object child : getPatternFilter().filter(viewer, compoundTask, setupTasks.toArray()))
+            {
+              if (select(viewer, compoundTask, child))
+              {
+                return true;
+              }
+            }
+
+            return false;
+          }
+
+          if (element instanceof PreferenceTask)
+          {
+            Boolean result = selected.get(element);
+            if (result != null)
+            {
+              return result;
+            }
+
+            PreferenceTask preferenceTask = (PreferenceTask)element;
+            String preferencePath = preferenceTask.getKey();
+            int index = preferencePath.indexOf('/', 1);
+            String scope = preferencePath.substring(1, index);
+            String keyWithoutScope = preferencePath.substring(index + 1);
+            index = keyWithoutScope.indexOf('/', 0);
+            String qualifier = keyWithoutScope.substring(0, index);
+            String key = keyWithoutScope.substring(index + 1);
+
+            // NOTE: this hideous mess is necessary because IPreferencesService.getString(...) doesn't do what it says on the tin.
+            // That is, it effectively IGNORES the contexts parameter and searches ALL scopes regardless of which ones were supplied.
+            // So it's impossible to obtain the instance, configuration or default preference value by respectively passing just
+            // InstanceScope.INSTANCE, ConfiguurationScope.INSTANCE or DefaultScope.INSTANCE - hence the need for the dummy scopes.
+            IScopeContext instanceScope = DUMMY_INSTANCE_SCOPE;
+            IScopeContext configurationScope = DUMMY_CONFIGURATION_SCOPE;
+            if (InstanceScope.SCOPE.equals(scope))
+            {
+              instanceScope = InstanceScope.INSTANCE;
+            }
+            else if (ConfigurationScope.SCOPE.equals(scope))
+            {
+              configurationScope = ConfigurationScope.INSTANCE;
+            }
+            preferenceContexts[1] = instanceScope;
+            preferenceContexts[2] = configurationScope;
+            String defaultValue = preferencesService.getString(qualifier, key, null, defaultContexts);
+            String preferenceValue = preferencesService.getString(qualifier, key, null, preferenceContexts);
+            boolean isSelected = !(preferenceValue == null || preferenceValue.equals(defaultValue));
+            selected.put(preferenceTask, isSelected);
+            return isSelected;
+          }
+
+          return true;
+        }
+      });
+      return treeControl;
+    }
+
+    /**
+    * @author Adrian Price
+    */
+    private static final class DummyPreferences implements IEclipsePreferences
+    {
+      public String absolutePath()
+      {
+        return null;
+      }
+
+      public void accept(IPreferenceNodeVisitor visitor) throws BackingStoreException
+      {
+      }
+
+      public void addNodeChangeListener(INodeChangeListener listener)
+      {
+      }
+
+      public void addPreferenceChangeListener(IPreferenceChangeListener listener)
+      {
+      }
+
+      public String[] childrenNames() throws BackingStoreException
+      {
+        return null;
+      }
+
+      public void clear() throws BackingStoreException
+      {
+      }
+
+      public void flush() throws BackingStoreException
+      {
+      }
+
+      public String get(String key, String def)
+      {
+        return def;
+      }
+
+      public boolean getBoolean(String key, boolean def)
+      {
+        return def;
+      }
+
+      public byte[] getByteArray(String key, byte[] def)
+      {
+        return def;
+      }
+
+      public double getDouble(String key, double def)
+      {
+        return def;
+      }
+
+      public float getFloat(String key, float def)
+      {
+        return def;
+      }
+
+      public int getInt(String key, int def)
+      {
+        return def;
+      }
+
+      public long getLong(String key, long def)
+      {
+        return def;
+      }
+
+      public String[] keys() throws BackingStoreException
+      {
+        return null;
+      }
+
+      public String name()
+      {
+        return null;
+      }
+
+      public Preferences node(String path)
+      {
+        return this;
+      }
+
+      public boolean nodeExists(String pathName) throws BackingStoreException
+      {
+        return false;
+      }
+
+      public Preferences parent()
+      {
+        return null;
+      }
+
+      public void put(String key, String value)
+      {
+      }
+
+      public void putBoolean(String key, boolean value)
+      {
+      }
+
+      public void putByteArray(String key, byte[] value)
+      {
+      }
+
+      public void putDouble(String key, double value)
+      {
+      }
+
+      public void putFloat(String key, float value)
+      {
+      }
+
+      public void putInt(String key, int value)
+      {
+      }
+
+      public void putLong(String key, long value)
+      {
+      }
+
+      public void remove(String key)
+      {
+      }
+
+      public void removeNode() throws BackingStoreException
+      {
+      }
+
+      public void removeNodeChangeListener(INodeChangeListener listener)
+      {
+      }
+
+      public void removePreferenceChangeListener(IPreferenceChangeListener listener)
+      {
+      }
+
+      public void sync() throws BackingStoreException
+      {
+      }
+    }
+
+    /**
+    * @author Adrian Price
+    */
+    private static final class DummyScope implements IScopeContext
+    {
+      private final String name;
+
+      public DummyScope(String name)
+      {
+        this.name = name;
+      }
+
+      public IPath getLocation()
+      {
+        return null;
+      }
+
+      public String getName()
+      {
+        return name;
+      }
+
+      public IEclipsePreferences getNode(String qualifier)
+      {
+        return DUMMY_PREFERENCES;
+      }
     }
   }
 }
