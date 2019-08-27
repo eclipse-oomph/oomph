cd872c157a548f2fe4868498dfe9c9595568f0d7
Ed Merks <ed.merks@gmail.com>
2019-08-27 14:54:09 +0200

    [Releng] Keep GitIndex.java in the repo so it can be used in a job

--- plugins/org.eclipse.oomph.setup.editor/src/org/eclipse/oomph/setup/presentation/GitIndex.java
+++ plugins/org.eclipse.oomph.setup.editor/src/org/eclipse/oomph/setup/presentation/GitIndex.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2018 Ed Merks (Berlin, Germany) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *    Ed Merks - initial API and implementation
+ */
 package org.eclipse.oomph.setup.presentation;
 
 import java.io.BufferedOutputStream;
