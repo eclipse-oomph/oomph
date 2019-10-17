2059dc5cd720bf92921853a77288158d89f88774
Ed Merks <ed.merks@gmail.com>
2019-10-17 10:37:58 +0200

    [551315] Register for an URI extension to start Oomph installer by clicking on an URI
    Provide support for marketplace.eclipse.org branding of the page.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=551315

--- releng/org.eclipse.oomph.releng/www/installer/fetch.php
+++ releng/org.eclipse.oomph.releng/www/installer/fetch.php
@@ -2,6 +2,10 @@
 header( 'Cache-control: no cache' );
 header('Content-type: application/xml');
 $url = htmlentities($_GET["url"]);
-$contents = file_get_contents($url);
+$contents = @file_get_contents($url);
+if ($url === FALSE) {
+http_response_code(404);
+} else {
 echo $contents;
+}
 ?>
