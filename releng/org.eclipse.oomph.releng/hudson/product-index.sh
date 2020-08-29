e22d4bc2fbd096e782266e29d0e5269ba4ea22e6
Ed Merks <ed.merks@gmail.com>
2020-08-29 10:47:09 +0200

    [Releng] Update how the product index is generated.

--- releng/org.eclipse.oomph.releng/hudson/product-index.sh
+++ releng/org.eclipse.oomph.releng/hudson/product-index.sh
@@ -26,26 +26,28 @@ for i in $named_releases $numbered_releases; do
     #ls eclipse-inst*
     for b in 32 64; do
       for os in win mac linux; do
-        if [ -f eclipse-inst-jre-$os$b.exe ]; then
-          candidate=eclipse-inst-jre-$os$b.exe
-        elif [ -f eclipse-inst-$os$b.exe ]; then
-          candidate=eclipse-inst-$os$b.exe
-        elif [ -f eclipse-inst-jre-$os$b.dmg ]; then
-          candidate=eclipse-inst-jre-$os$b.dmg
-        elif [ -f eclipse-inst-$os$b.dmg ]; then
-          candidate=eclipse-inst-$os$b.dmg
-        elif [ -f eclipse-inst-jre-$os$b.tar.gz ]; then
-          candidate=eclipse-inst-jre-$os$b.tar.gz
-        elif [ -f eclipse-inst-$os$b.tar.gz ]; then
-          candidate=eclipse-inst-$os$b.tar.gz
-        else
-          candidate=""
-        fi
+        for jre in "true" "false"; do
+          if [[ $jre == "false ]]; then
+            prefix="eclipse-inst-"
+          else
+            prefix="eclipse-inst-jre-"
+          fi
 
-        if [ "$candidate" != "" ]; then
-          #echo "  >> $i/$j/$(ls $candidate)"
-          echo "  <installer release=\"$i/$j\" os=\"$os\" bitness=\"$b\" url=\"https://www.eclipse.org/downloads/download.php?file=/oomph/epp/$i/$j/$candidate\"/>"
-        fi
+          if [ -f $prefix$os$b.exe ]; then
+            candidate=$prefix$os$b.exe
+          elif [ -f $prefix$os$b.dmg ]; then
+            candidate=$prefix$os$b.dmg
+          elif [ -f $prefix$os$b.tar.gz ]; then
+            candidate=$prefix$os$b.tar.gz
+          else
+            candidate=""
+          fi
+
+          if [ "$candidate" != "" ]; then
+            #echo "  >> $i/$j/$(ls $candidate)"
+            echo "  <installer release=\"$i/$j\" os=\"$os\" bitness=\"$b\" jre=\"$jre\"url=\"https://www.eclipse.org/downloads/download.php?file=/oomph/epp/$i/$j/$candidate\"/>"
+          fi
+        done
       done
     done
   done
@@ -55,5 +57,3 @@ for i in $named_releases $numbered_releases; do
 done
 
 echo "</index>"
-
-
