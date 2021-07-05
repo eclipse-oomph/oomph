3d29572892a81b0877c01b4b661ba38d2c312b90
Ed Merks <ed.merks@gmail.com>
2021-07-05 09:51:38 +0200

    [574646] Installer Auto Launch button template for GitHub
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=574646
    
    Server doesn't allow archives to be read, so use an unzipped cache.

--- releng/org.eclipse.oomph.releng/www/installer/fetch.php
+++ releng/org.eclipse.oomph.releng/www/installer/fetch.php
@@ -4,19 +4,16 @@ header('Content-type: application/xml');
 $url = $_GET["url"];
 if (preg_match("@^https?://[^/]+eclipse.org/@i", $url) == FALSE) {
   $path = str_replace("://", "/", $url);
-  $zip = new ZipArchive;
-  $res = $zip->open('../setups.zip');
-  if ($res === TRUE) {
-    $contents = $zip->getFromName($path);
-    if ($contents == "") {
-      $path = str_replace("http://", "https/", $url);
-      $contents = $zip->getFromName($path);
-    }
-    $zip->close();
-    echo $contents;
-  } else {
-    http_response_code(404);
+  $contents = @file_get_contents("../setups/$path");
+  if ($contents == "") {
+    $path = str_replace("http://", "https/", $url);
+    $contents = @file_get_contents("../setups/$path");
   }
+  if ($contents == "") {
+      http_response_code(404);
+  } else {
+      echo $contents;
+   }
 } else {
   $contents = @file_get_contents($url);
   if ($contents === FALSE) {
