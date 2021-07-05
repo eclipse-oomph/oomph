ff66f6cdbe14777e6cbfc6c0010225f54ff36c21
Ed Merks <ed.merks@gmail.com>
2021-07-05 08:43:54 +0200

    [574646] Installer Auto Launch button template for GitHub
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=574646
    
    Also update the installer page to show configuration documentation by
    extracting it from the setups.zip.

--- releng/org.eclipse.oomph.releng/www/installer/fetch.php
+++ releng/org.eclipse.oomph.releng/www/installer/fetch.php
@@ -2,10 +2,27 @@
 header( 'Cache-control: no cache' );
 header('Content-type: application/xml');
 $url = $_GET["url"];
-$contents = @file_get_contents($url);
-if ($contents === FALSE) {
-http_response_code(404);
+if (preg_match("@^https?://[^/]+eclipse.org/@i", $url) == FALSE) {
+  $path = str_replace("://", "/", $url);
+  $zip = new ZipArchive;
+  $res = $zip->open('../setups.zip');
+  if ($res === TRUE) {
+    $contents = $zip->getFromName($path);
+    if ($contents == "") {
+      $path = str_replace("http://", "https/", $url);
+      $contents = $zip->getFromName($path);
+    }
+    $zip->close();
+    echo $contents;
+  } else {
+    http_response_code(404);
+  }
 } else {
-echo $contents;
+  $contents = @file_get_contents($url);
+  if ($contents === FALSE) {
+    http_response_code(404);
+  } else {
+   echo $contents;
+  }
 }
 ?>
