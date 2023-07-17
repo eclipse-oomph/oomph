b5f9e73a6894c610b4ae0f570fc472e9fadb567b
Ed Merks <ed.merks@gmail.com>
2023-07-17 12:44:00 +0200

    Uninstall org.eclipse.mylyn.commons.repositories.http.feature.group

--- plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/ProductCatalogGenerator.java
+++ plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/ProductCatalogGenerator.java
@@ -194,6 +194,7 @@ public class ProductCatalogGenerator implements IApplication
       "org.eclipse.mylyn.commons.feature.group", //
       "org.eclipse.mylyn.commons.notifications.feature.group", //
       "org.eclipse.mylyn.commons.repositories.feature.group", //
+      "org.eclipse.mylyn.commons.repositories.http.feature.group", //
       "org.eclipse.mylyn.git.feature.group", //
       "org.eclipse.mylyn.hudson.feature.group", //
       "org.eclipse.mylyn.ide_feature.feature.group", //
