f2ff53bd1477d16a8fe67d6afcd1ab286cd13845
Ed Merks <ed.merks@gmail.com>
2023-11-17 12:50:17 +0100

    Test 2023-12 with for the marketplace

--- plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/MarketplaceCatalogGenerator.java
+++ plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/MarketplaceCatalogGenerator.java
@@ -327,7 +327,7 @@ public class MarketplaceCatalogGenerator implements IApplication
     // platformVersions.put("platform.version=4.13", "2019-09");
     // platformVersions.put("platform.version=4.14", "2019-12");
     // platformVersions.put("platform.version=4.15", "2020-03");
-    platformVersions.put("platform.version=4.16", "2020-06");
+    // platformVersions.put("platform.version=4.16", "2020-06");
     platformVersions.put("platform.version=4.17", "2020-09");
     platformVersions.put("platform.version=4.18", "2020-12");
     platformVersions.put("platform.version=4.19", "2021-03");
@@ -341,6 +341,7 @@ public class MarketplaceCatalogGenerator implements IApplication
     platformVersions.put("platform.version=4.27", "2023-03");
     platformVersions.put("platform.version=4.28", "2023-06");
     platformVersions.put("platform.version=4.29", "2023-09");
+    platformVersions.put("platform.version=4.30", "2023-12");
 
     Set<URI> nodeQueryURIs = new LinkedHashSet<URI>();
     {
