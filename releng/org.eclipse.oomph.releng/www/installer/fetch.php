75c3fd986cb273131cb85c385f9775828ef5a033
Ed Merks <ed.merks@gmail.com>
2023-11-27 16:40:31 +0100

    Enhance the intaller/fetch.php to look for all setups in the local cache

--- releng/org.eclipse.oomph.releng/www/installer/fetch.php
+++ releng/org.eclipse.oomph.releng/www/installer/fetch.php
@@ -2,24 +2,20 @@
 header( 'Cache-control: no cache' );
 header('Content-type: application/xml');
 $url = $_GET["url"];
-if (preg_match("@^https?://[^/]+eclipse.org/@i", $url) == FALSE) {
-  $path = str_replace("://", "/", $url);
+$path = str_replace("://", "/", $url);
+$contents = @file_get_contents("../setups/$path");
+if ($contents == "") {
+  $path = str_replace("http://", "https/", $url);
   $contents = @file_get_contents("../setups/$path");
-  if ($contents == "") {
-    $path = str_replace("http://", "https/", $url);
-    $contents = @file_get_contents("../setups/$path");
-  }
-  if ($contents == "") {
-      http_response_code(404);
-  } else {
-      echo $contents;
-   }
-} else {
+}
+
+if ($contents == "") {
   $contents = @file_get_contents($url);
-  if ($contents === FALSE) {
-    http_response_code(404);
-  } else {
-   echo $contents;
-  }
+}
+
+if ($contents == "" || $contents === FALSE) {
+  http_response_code(404);
+} else {
+  echo $contents;
 }
 ?>
