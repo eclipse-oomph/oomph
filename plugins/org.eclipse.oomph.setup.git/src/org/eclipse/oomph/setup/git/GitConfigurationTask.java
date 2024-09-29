50845ac2fbc0457b874abba4d0200e99fede4efb
Ed Merks <ed.merks@gmail.com>
2024-09-29 11:39:07 +0200

    Fix missing copyright headers

--- plugins/org.eclipse.oomph.setup.git/src/org/eclipse/oomph/setup/git/GitConfigurationTask.java
+++ plugins/org.eclipse.oomph.setup.git/src/org/eclipse/oomph/setup/git/GitConfigurationTask.java
@@ -1,4 +1,9 @@
 /**
+ * Copyright (c) Eclispe contributors and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
  */
 package org.eclipse.oomph.setup.git;
 
