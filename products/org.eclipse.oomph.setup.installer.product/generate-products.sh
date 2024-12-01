5ec4ece00fd798398e59e7ab59304ff50fc14c00
Ed Merks <ed.merks@gmail.com>
2024-12-01 16:44:10 +0100

    Stop produce restricted installer products without a JRE
    - The restricted products are used primarily download.eclipse.org and
    there only the ones with a JRE are used.
    - This will save time building and especially mac signing and
    notarization.

--- products/org.eclipse.oomph.setup.installer.product/generate-products.sh
+++ products/org.eclipse.oomph.setup.installer.product/generate-products.sh
@@ -9,7 +9,7 @@ set -x
 cp Installer.p2.inf Installer-With-JRE.p2.inf
 sed -e 's/uid="\([^"]*\)"/uid="\1.with-jre"/;s/<!--//;s/-->//' < Installer.product > Installer-With-JRE.product
 
-for i in Installer Installer-With-JRE; do 
+for i in Installer-With-JRE; do 
   echo $i;
   cp $i.p2.inf $i-Restricted.p2.inf
   sed -e 's/uid="\([^"]*\)"/uid="\1.restricted"/' < $i.product > $i-Restricted.product
