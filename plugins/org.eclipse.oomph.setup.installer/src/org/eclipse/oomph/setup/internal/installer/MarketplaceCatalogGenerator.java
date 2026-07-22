0838190a860ee2d22c65df3c9deb5ee4fe575c4f
Ed Merks <ed.merks@gmail.com>
2026-07-22 15:55:39 +0200

    Test marketplace for 2026-09

--- plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/MarketplaceCatalogGenerator.java
+++ plugins/org.eclipse.oomph.setup.installer/src/org/eclipse/oomph/setup/internal/installer/MarketplaceCatalogGenerator.java
@@ -385,7 +385,7 @@ public class MarketplaceCatalogGenerator implements IApplication
     platformVersions.put("platform.version=4.38", "2025-12");
     platformVersions.put("platform.version=4.39", "2026-03");
     platformVersions.put("platform.version=4.40", "2026-06");
-    platformVersions.put("platform.version=4.41", "2026-06");
+    platformVersions.put("platform.version=4.41", "2026-09");
 
     Set<URI> nodeQueryURIs = new LinkedHashSet<URI>();
     {
